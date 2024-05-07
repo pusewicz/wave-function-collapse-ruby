5d4797730f263707ee7f797856ba5cd064a98f40
Piotr Usewicz <piotr@layer22.com>
2024-05-07 07:54:46 +0200

    Add the debug gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "wave_function_collapse"
+require "debug"
 require "minitest/autorun"
 
 Minitest::Test.send(:include, WaveFunctionCollapse)
